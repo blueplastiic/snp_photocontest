f2d7f5f265cc02bf330f8a090eb75e1982b21480
Bogdan <bgdnvlkvb7@gmail.com>
2025-09-07 20:21:23 +0300

    small refactoring

--- users/services.py
+++ users/services.py
@@ -2,6 +2,14 @@ from service_objects.services import Service
 from rest_framework.authtoken.models import Token
 from .models import User
 
+class ConfirmActionService(Service):
+    def process(self): #pyright: ignore
+        user = self.data.get('user', None)
+        password = self.data.get('password', None)
+        if user and user.check_password(password):
+            return True
+        return False
+
 class RegisterUserService(Service):
     def process(self): #change change change
         email = self.data['email']
@@ -11,41 +19,6 @@ class RegisterUserService(Service):
 
         new_user = User.objects.create_user(email=email, username=username, password=password, about=about) #pyright: ignore
         return new_user 
-
-class ConfirmActionService(Service):
-    def process(self): #pyright: ignore
-        user = self.data.get('user', None)
-        password = self.data.get('password', None)
-        if user and user.check_password(password):
-            return True
-        return False
-
-class UpdatePublicUserInfoService(Service):
-    def process(self): #pyright: ignore
-        user = self.data.get('user')
-        if not user:
-            raise ValueError('Invalid user instance')
-
-        username = self.data.get('username')
-        about = self.data.get('about')
-        if not username and not about:
-            raise ValueError('Data not provided')
-
-        if username:
-            user.username = username
-        if about:
-            user.about = about
-
-        user.save()
-
-class GetUserByIdService(Service):
-    def process(self): #pyright: ignore
-        user_id = self.data['user_id']
-        try:
-            user = User.objects.get(id=user_id)
-        except User.DoesNotExist:
-            raise ValueError('Invalid id,user not found') #not sure about that one
-        return user
 
 class GetUserTokenService(Service): 
     def get_token(self, user):
@@ -68,7 +41,7 @@ class CreateUserTokenService(Service):
     def process(self): #pyright: ignore
         user = self.data.get('user', None)
         if not user:
-            raise ValueError('Invalid user instance')
+            raise ValueError('User not authenticated')
         token,_ = Token.objects.get_or_create(user=user)
         return token
 
@@ -76,7 +49,7 @@ class DeleteUserTokenService(Service):
     def process(self):
         user = self.data.get('user', None)
         if not user:
-            raise ValueError('Invalid user instance')
+            raise ValueError('User cannot be found ')
         try: 
             token = Token.objects.get(user=user)
             token.delete()
@@ -86,9 +59,38 @@ class DeleteUserTokenService(Service):
 class UpdateUserTokenService(Service): #transaction?
     def process(self):
         user = self.data.get('user', None)
+        if not user:
+            raise ValueError('User cannot be found')
         DeleteUserTokenService.execute({'user': user})
         new_token = CreateUserTokenService.execute({'user': user})
         return new_token
+
+class GetUserByIdService(Service):
+    def process(self): #pyright: ignore
+        user_id = self.data['user_id']
+        try:
+            user = User.objects.get(id=user_id)
+        except User.DoesNotExist:
+            raise ValueError('Invalid id,user not found') #not sure about that one
+        return user
+
+class UpdatePublicUserInfoService(Service):
+    def process(self): #pyright: ignore
+        user = self.data.get('user')
+        if not user:
+            raise ValueError('User cannot be found')
+
+        username = self.data.get('username')
+        about = self.data.get('about')
+        if not username and not about:
+            raise ValueError('Data not provided')
+
+        if username:
+            user.username = username
+        if about:
+            user.about = about
+
+        user.save()
 
 class DeleteUserService(Service):
     def process(self): #pyright: ignore
--- users/views.py
+++ users/views.py
@@ -23,7 +23,10 @@ class UserUpdateTokenAPIView(APIView):
 
     def post(self, request): #not sure about the http-method
         user = request.user
-        new_token = UpdateUserTokenService.execute({'user': user})
+        try:
+            new_token = UpdateUserTokenService.execute({'user': user})
+        except ValueError:
+            return Response(data={'error': 'User cannot be found'},status=status.HTTP_404_NOT_FOUND)
 
         response_data = {'id': user.id, 'token': new_token.key} #pyright: ignore
         response_serializer = UserGetTokenSerializer(response_data)
