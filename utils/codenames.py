e1feb0239f90f55a58d25d486cd3868832069706
Bogdan <bgdnvlkvb7@gmail.com>
2025-09-10 17:44:32 +0300

    created utils module to follow snp convention

--- utils/codenames.py
+++ utils/codenames.py
@@ -1,8 +1,10 @@
 permission_codenames= {'participants': ['add_photo','change_photo','delete_photo','view_photo',
                                          'add_comment', 'change_comment', 'delete_comment', 'view_comment',
-                                         'add_vote', 'delete_vote'],
+                                         'add_vote', 'delete_vote'
+                                        ],
 
                        'moderators': ['change_status','delete_photo', 'view_photo',
-                                        'view_comment', 'delete_comment']}
-
+                                        'view_comment', 'delete_comment'
+                                      ]
+                       }
 
