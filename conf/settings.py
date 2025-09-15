0b3d38e9a9014773f3d1e11989bb21178d5544cb
Bogdan <bgdnvlkvb7@gmail.com>
2025-09-15 17:36:26 +0300

    fixed auth model name in settings.py

--- conf/settings.py
+++ conf/settings.py
@@ -130,7 +130,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-AUTH_USER_MODEL = 'users.User'
+AUTH_USER_MODEL = 'models_app.user.models.User'
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
