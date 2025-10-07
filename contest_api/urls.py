e6215c88dd1ce520b9aac55275b24bcf8243fac7
Bogdan <bgdnvlkvb7@gmail.com>
2025-10-07 16:37:47 +0300

    responsability fixes in vote services

--- contest_api/urls.py
+++ contest_api/urls.py
@@ -1,13 +1,17 @@
 from django.urls import path
 from contest_api.views.photo.create import CreatePhotoAPIView
 from contest_api.views.photo.detail import PhotoDetailAPIView
-from contest_api.views.vote.views import VoteAPIView
+
+from contest_api.views.vote.create import CreateVoteAPIView
+from contest_api.views.vote.delete import DeleteVoteAPIView
 
 app_name = 'contest_api'
 
 urlpatterns=[ 
     path('photo/<int:id>/', PhotoDetailAPIView.as_view(), name='photo_detail'),
     path('photo/create/', CreatePhotoAPIView.as_view(), name='photo_create'),
-    path('photo/<int:id>/vote', VoteAPIView.as_view(), name='vote'),
+
+    path('vote/', CreateVoteAPIView.as_view(), name='vote_create'),
+    path('vote/<int:vote_id>/', DeleteVoteAPIView.as_view(), name='vote_delete') #but how do we actually get the vote id?
 ]
 
--- contest_api/views/vote/delete.py
+++ contest_api/views/vote/delete.py
@@ -1,28 +1,20 @@
 from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
+
 from service_objects.services import  ServiceOutcome
 from contest_api.services import CreateVoteService,DeleteVoteService
 
-class VoteAPIView(APIView):
+class DeleteVoteAPIView(APIView):
     permission_classes = [IsAuthenticated]
-
-    def post(self,request, *arg, **kwargs):
-        outcome: ServiceOutcome = ServiceOutcome(
-            CreateVoteService,
-            {
-                **kwargs, 'user': request.user
-            }
-        )
-        
-        return Response(outcome.response_status)
 
     def delete(self, request, *args, **kwargs):
         outcome: ServiceOutcome = ServiceOutcome(
             DeleteVoteService,
             {
-                **kwargs, 'user': request.user
+                **kwargs, **request.data, 'user': request.user
             }
         )
+
         return Response(outcome.response_status)
 
