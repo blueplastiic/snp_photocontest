864c1bef9efe4d917f1a07302dec4fe868b26a84
Bogdan <bgdnvlkvb7@gmail.com>
2025-10-09 13:42:09 +0300

    retrieve user service fix

--- users_api/services/user/retrieve.py
+++ users_api/services/user/retrieve.py
@@ -1,20 +1,40 @@
+from typing import Self, Optional
+
 from service_objects.errors import NotFound
 from service_objects.services import ServiceWithResult
-from service_objects.errors import NotFound
+
 from models_app.models import User
+
 from django import forms
+from django.core.exceptions import ObjectDoesNotExist
 
-class GetUserByIdService(ServiceWithResult):
-    id = forms.IntegerField()
+class RetrieveUserService(ServiceWithResult):
+    user_id = forms.IntegerField()
 
-    def process(self): #pyright: ignore
-        id = self.cleaned_data.get('id')
+    custom_validations = ['user_presence']
 
-        try:
-            user = User.objects.get(id=id)
-        except User.DoesNotExist:
-            raise NotFound(additional_info='User does not exist')
-
-        self.result = user
+    def process(self) -> Self: #pyright: ignore
+        self.run_custom_validations()
+        if self.is_valid():
+            self.result = self._user
         return self
+
+    @property
+    def _user(self) -> Optional[User]:
+        try:
+            return User.objects.get(
+                id = self.cleaned_data['user_id']
+            )
+        except ObjectDoesNotExist:
+            return None
+            
+
+    def user_presence(self) -> None:
+        if not self._user:
+            self.add_error(
+                'user_id',
+                NotFound(
+                    message=f"User {self.cleaned_data['user_id']} does not exist"
+                )
+            )
 
