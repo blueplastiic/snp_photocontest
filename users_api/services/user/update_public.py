83036e6caecf91400116af0d01655f696fa0e68f
Bogdan <bgdnvlkvb7@gmail.com>
2025-10-09 13:57:04 +0300

    update public data user service fix

--- users_api/services/user/update_public.py
+++ users_api/services/user/update_public.py
@@ -1,31 +1,57 @@
+from typing import Self
+from functools import lru_cache
+
 from django import forms
+
+from models_app.models import User
+
 from service_objects.services import ServiceWithResult
 from service_objects.fields import ModelField
 from service_objects.errors import ValidationError
-from models_app.models import User
 
 class UpdatePublicInfoService(ServiceWithResult):
     user = ModelField(User)
     username = forms.CharField(max_length=30, required=False)
     about = forms.CharField(max_length=500, required=False)
 
-    def process(self): #pyright: ignore
-        user = self.cleaned_data.get('user')
+    custom_validations=['data_provided']
 
-        username = self.cleaned_data.get('username')
-        about = self.cleaned_data.get('about')
-
-        if not username and not about: 
-            raise ValidationError('Data not provided')
-
-        if username:
-            if User.objects.filter(username=username).exclude(id=user.id).exists(): #pyright: ignore
-                raise ValidationError('This username is already taken')
-            user.username = username #pyright: ignore
-        if about:
-            user.about = about #pyright:ignore
-
-        user.save() #pyright:ignore
-        self.result = user
+    def process(self) -> Self: #pyright: ignore
+        self.run_custom_validations()
+        if self.is_valid():
+            if self.cleaned_data.get('username'):
+                self.update_username()
+            if self.cleaned_data.get('about'):
+                self.update_about()
         return self
+
+    @property
+    @lru_cache()
+    def _user(self) -> User:
+        return self.cleaned_data['user']
+
+    def data_provided(self) -> None:
+        if not self.cleaned_data and not self.about:
+            self.add_error(
+                'username',
+                ValidationError(
+                    message="Neither username nor about field was provided"
+                )
+            )
+            self.add_error(
+                'about',
+                ValidationError(
+                    message="Neither username nor about field was provided"
+                )
+            )
+
+    def update_username(self) -> None:
+        user = self._user
+        user.username = self.cleaned_data['username']
+        user.save()
+
+    def update_about(self) -> None:
+        user = self._user
+        self.about = self.cleaned_data['about']
+        user.save
 
