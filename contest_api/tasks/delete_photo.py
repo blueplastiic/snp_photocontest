344a979f23e91af5e8ced05d00e865ed1a494b2e
Bogdan <bgdnvlkvb7@gmail.com>
2025-10-20 15:52:09 +0300

    wrapped task in try-exc

--- contest_api/tasks/delete_photo.py
+++ contest_api/tasks/delete_photo.py
@@ -1,3 +1,5 @@
+from django.core.exceptions import ObjectDoesNotExist
+
 from utils.statuses import PhotoStatus
 
 from models_app.models import Photo
@@ -5,13 +7,13 @@ from models_app.models import Photo
 from celery import shared_task
 
 
-@shared_task
+@shared_task(bind=True)
 def delete_photo_task(photo_id: int) -> None:
-    photo = Photo.objects.get(id=photo_id)
-
-    #user has time to cancel the deletion 
-    #which is basically returning status to 'approved'
-    #if not, then we BALL:
-    if photo.status == PhotoStatus.DELETED:
-        photo.delete()
+    try:
+        photo = Photo.objects.get(id=photo_id)
+        if photo.status == PhotoStatus.DELETED:
+            photo.delete()
+    except ObjectDoesNotExist as exc: 
+        pass
+        #TODO: logging
     
