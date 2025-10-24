35ef9b2a2a50bf918644e8d2f490f8a9b1f7a86c
Bogdan <bgdnvlkvb7@gmail.com>
2025-10-24 18:10:40 +0300

    removed redundant aliases in users_api urls

--- users_api/urls.py
+++ users_api/urls.py
@@ -5,13 +5,13 @@ from users_api.views.token import UpdateTokenAPIView
 
 from rest_framework.authtoken.views import obtain_auth_token
 
-urlpatterns=[
-     path('register/', CreateUserAPIView.as_view(), name='user_register'),
-     path('login/', obtain_auth_token, name='user_login'),
+urlpatterns=[ 
+    path('register/', CreateUserAPIView.as_view()),
+    path('login/', obtain_auth_token),
+
+    path('<int:id>/', RetrieveUserAPIView.as_view()),
     
-     path('<int:id>/', RetrieveUserAPIView.as_view(), name='user_profile'),
-    
-     path('me/', RetrieveUpdateDeleteUserAPIView.as_view(), name='user_settings'),
+    path('me/', RetrieveUpdateDeleteUserAPIView.as_view()),
     path('me/token/', UpdateTokenAPIView.as_view(),) 
 ]
 
