21f2525a9af0ea988538bcc5648597cd5d4c9ead
Bogdan <bgdnvlkvb7@gmail.com>
2025-10-28 13:28:24 +0300

    inherited base obtain auth token apiview so it can be documented

--- users_api/views/user/__init__.py
+++ users_api/views/user/__init__.py
@@ -1,2 +1,1 @@
-from .views import RetrieveUserAPIView, RetrieveUpdateDeleteUserAPIView, CreateUserAPIView
-
+from .views import RetrieveUserAPIView, RetrieveUpdateDeleteUserAPIView, CreateUserAPIView, LoginUserAPIView
